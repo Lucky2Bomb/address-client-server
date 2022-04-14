ce9da3e8802c6e2688457c74bd350c35ad278ce6
Lucky2Bomb <Lucky2Bomb@gmail.com>
2022-04-15 03:29:18 +0500

    router hub

---
+++ server/src/routes/index.ts
@@ -0,0 +1,8 @@
+import addressRouter from "./address.router";
+import citizenRouter from "./citizen.router";
+import cityRouter from "./city.router";
+import seedRouter from "./seed.router";
+
+export default [
+    cityRouter, citizenRouter, seedRouter, addressRouter
+];
