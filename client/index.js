23654875f1e66ce48b5d9882f218adec441e9e3f
Lucky2Bomb <Lucky2Bomb@gmail.com>
2022-04-21 23:12:46 +0500

    added port in config file

--- client/index.js
+++ client/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const { CLIENT_PORT } = require('./src/config.json');
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'build')));
@@ -7,5 +8,6 @@ app.use(express.static(path.join(__dirname, 'build')));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
-
-app.listen(80);
+app.listen(CLIENT_PORT, () => {
+  console.log("client server started at http://localhost:" + CLIENT_PORT);
+});
